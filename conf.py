e3f9e45eaf37bbef334b30e1e49955a48a997ece
Rok Garbas <rok@garbas.si>
2010-09-15 08:56:28 +0200

    excluding sphinx-to-github folder

--- conf.py
+++ conf.py
@@ -68,7 +68,7 @@ release = '0.1'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = ['_build', 'plone-2.0.5', 'plone-4.0', 'lib', 'bin',
-                    'include']
+                    'include', 'sphinx-to-github']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
